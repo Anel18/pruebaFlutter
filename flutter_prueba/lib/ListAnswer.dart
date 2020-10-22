7bec883921838e5718827a93d5da820094151b43
UrielCaricio <uri.aguila.10@gmail.com>
2020-10-22 11:49:29 -0500

    filtro de busqueda por UserId

--- flutter_prueba/lib/ListAnswer.dart
+++ flutter_prueba/lib/ListAnswer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_prueba/Answers.dart';
 import 'package:flutter_prueba/CardAnswers.dart';
 
@@ -12,16 +13,17 @@ class ListAnswer extends StatefulWidget{
 
   class ListAnswerState extends State<ListAnswer>{
   List<Answer>answers=Answer.answers();
+  List<Answer>_list=Answer.answers();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return ListView.builder(
       shrinkWrap: true,
-        itemCount: answers.length,
+        itemCount: _list.length+1,
         itemBuilder: (BuildContext context,int index){
-          return Dismissible(
-              key: ObjectKey(answers[index]),
-              child: CardAnswers(answers[index]),
+          return index==0 ? _searchBar() : Dismissible(
+              key: ObjectKey(answers[index-1]),
+              child: CardAnswers(answers[index-1]),
           onDismissed: (direction){
             setState(() {
               answers.removeAt(index);
@@ -30,5 +32,26 @@ class ListAnswer extends StatefulWidget{
           );
         });
   }
+
+  _searchBar() {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+
+    child: TextField(
+        decoration: InputDecoration( hintText: 'Search') ,
+      onChanged: (text){
+          text=text.toLowerCase();
+          setState(() {
+            _list=answers.where((note){
+              var noteTitle= note.userId.toLowerCase();
+              return noteTitle.contains(text);
+            }).toList();
+          });
+      },
+    ),
+
+
+    );}
+  
   
   }
