06e7f23f6850ddada267dfb6222658cb2b306d5b
UrielCaricio <uri.aguila.10@gmail.com>
2020-10-22 00:21:00 -0500

    creacion proyecto

---
+++ flutter_prueba/android/app/src/main/kotlin/com/cynthia/flutter_prueba/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cynthia.flutter_prueba
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pru_flutter/android/app/src/main/kotlin/com/example/pru_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pru_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
