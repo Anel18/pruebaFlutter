418519c67967d6e27ac32e297fc57683ba043302
UrielCaricio <uri.aguila.10@gmail.com>
2020-10-22 01:25:11 -0500

    creacion card

---
+++ flutter_prueba/lib/Answers.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class Answer{
+   String answer;
+   String guideId;
+   String questionId;
+   String  right;
+   String studentEventId;
+   String time;
+   String timestamp;
+   String userId;
+Answer(this.answer,this.guideId,this.questionId,this.right,this.studentEventId,this.time,this.timestamp,this.userId);
+
+static List<Answer>answers(){
+  return[Answer("individual, this, that, size, classifier for people or objects in genera","HSK1_en"
+      ,"32","true","-L9ha9C2SBY_RPDGQ-E7","2001","1523330884586","QUbCBEv4BARZ84PIVUbCz5GhRZp2"),Answer("tener, hay, hay, existir, ser","HSK1_en"
+      ,"23","false","-L9ha9C2SBY_RPDGQ-E7","1256","1523330885884","QUbCBEv4BARZ84PIVUbCz5GhRZp2"),Answer("minuto","HSK1_en"
+      ,"124","false","-L9ha9C2SBY_RPDGQ-E7","2258","1523330888132","QUbCBEv4BARZ84PIVUbCz5GhRZp2")];
+}
+
+}
---
+++ flutter_prueba/lib/CardAnswers.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_prueba/Answers.dart';
+
+class CardAnswers extends StatelessWidget{
+  Answer answer;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return Card(
+     child: Column(
+       children: <Widget>[
+         Container(
+           height: 144.0,
+           width: 500.0
+         ),
+         padding(Text(answer.questionId, style: TextStyle(fontSize: 18.0))),
+         Row(children:<Widget> [
+           padding(Icon(Icons.question_answer_outlined)),
+           padding(Text(answer.answer, style: TextStyle(fontSize: 18.0)))
+         ],)
+
+       ],
+     ),
+   )
+  }
+
+  Widget padding(Widget widget){
+    return Padding(padding: EdgeInsets.all(7.0),child: widget);
+  }
+
+}
