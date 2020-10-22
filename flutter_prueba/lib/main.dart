af4c16fa1032ce1860b9277f01d730f0e8256ad7
UrielCaricio <uri.aguila.10@gmail.com>
2020-10-22 02:29:19 -0500

    se quito el boton de agregar

--- flutter_prueba/lib/main.dart
+++ flutter_prueba/lib/main.dart
@@ -79,11 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: ListAnswer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
